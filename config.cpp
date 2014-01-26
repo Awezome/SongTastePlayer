ee37f9b0d9eed8b516c9281f724ef6874736b6b0
Awesomez <likezyp@gmail.com>
2014-01-26 12:03:34 +0800

    ver 1.3

--- config.cpp
+++ config.cpp
@@ -3,7 +3,7 @@
 QString Config::title="SongTaste Player";
 QString Config::homepage="http://songtasteplayer.lofter.com/";
 QString Config::changelog="http://songtasteplayer.lofter.com/post/2fc8fe_cfb6f6";
-QString Config::version="1.2.20140125";
+QString Config::version="1.3.20140126";
 
 
 QString Config::stindex="http://www.songtaste.com/music/rec/lan";
